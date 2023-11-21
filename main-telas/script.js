e0ad99d5604d146314a591a7195660ab23584aa6
vifp <vfpaiola98@gmail.com>
2023-11-21 17:29:11 -0300

    Correção tela login

--- main-telas/script.js
+++ main-telas/script.js
@@ -2,10 +2,57 @@ const container = document.getElementById('container');
 const registerBtn = document.getElementById('register');
 const loginBtn = document.getElementById('login');
 
+const formularioCadastro = document.querySelector('.form-cadastro');
+const btnEntrar = document.querySelector('.btn-entrar');
+const btnCadastrar = document.querySelector('.btn-cadastrar');
+
+// Cadastro
+const inputNome = document.querySelector('.nome');
+const inputCPF = document.querySelector('.cpf');
+const inputEmailCad = document.querySelector('.email-cadastro');
+const inputSenhaCad = document.querySelector('.senha-cadastro');
+// Entrar
+const inputEmail = document.querySelector('.email-login');
+const inputSenha = document.querySelector('.senha-login');
+
+
 registerBtn.addEventListener('click', () => {
     container.classList.add("active");
 });
 
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
+});
+
+btnEntrar.addEventListener('click', () => {
+    console.log("CLICOU");
+});
+
+function cadastrar() {
+    fetch("http://localhost:8080/",
+    {
+        method: "POST",
+        headers : {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        },
+        
+        body: JSON.stringify({
+            nome: inputNome.value,
+            cpf: inputCPF.value,
+            email: inputEmailCad.value,
+            senha: inputSenhaCad.value
+        }),
+    })
+    .then(function (res) {console.log(res)})
+    .catch(function (error) {
+        console.error('Erro ao cadastrar: ', error);
+    });
+};
+
+
+
+formularioCadastro.addEventListener('submit', function(event) {
+    event.preventDefault();
+    cadastrar();
 });
