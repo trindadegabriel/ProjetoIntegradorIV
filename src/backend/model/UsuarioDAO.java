f1e719bbfe07e31744180b32955f4c4db82a4217
RenatoWlk <renatowilkerpaulasilva9@gmail.com>
2023-12-02 22:06:06 -0300

    Cadastro, Login e melhorias

--- src/backend/model/UsuarioDAO.java
+++ src/backend/model/UsuarioDAO.java
@@ -4,52 +4,75 @@ import backend.bd.GerenciadorDeConexao;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Filters;
 import com.mongodb.client.result.DeleteResult;
+import com.mongodb.client.result.InsertOneResult;
 import com.mongodb.client.result.UpdateResult;
-import com.mongodb.client.MongoCollection;
 import org.bson.Document;
 import org.bson.conversions.Bson;
 
 public class UsuarioDAO {
     private MongoDatabase database;
-    private MongoCollection<Document> colecaoUsuario;
 
     public UsuarioDAO(){
         this.database = GerenciadorDeConexao.conectar();
-        this.colecaoUsuario = database.getCollection("usuario");
     }
 
-    public void inserirUsuario(Usuario usuario){
-        Document documento = new Document("nome",  usuario.getNome())
-                                  .append("cpf",   usuario.getCpf())
-                                  .append("email", usuario.getEmail())
-                                  .append("senha", usuario.getSenha());
-        colecaoUsuario.insertOne(documento);
-        System.out.println("Usuário adicionado\n");
+    public boolean inserirUsuario(Usuario usuario){
+        Document documento = new Document()
+            .append("nome", usuario.getNome())
+            .append("cpf", usuario.getCpf())
+            .append("email", usuario.getEmail())
+            .append("senha", usuario.getSenha());
+
+        InsertOneResult resultado = database.getCollection("usuarios").insertOne(documento);
+        return resultado.wasAcknowledged();
     }
 
-    public void alterarUsuario(Usuario usuario){
+    public boolean alterarUsuario(Usuario usuario){
         Bson filtro = Filters.eq("cpf", usuario.getCpf());
         Document novoUsuario = new Document("$set", new Document("nome",  usuario.getNome())
                                                              .append("email", usuario.getEmail())
                                                              .append("senha", usuario.getSenha()));
 
-        UpdateResult resultado = colecaoUsuario.updateOne(filtro, novoUsuario);
+        UpdateResult resultado = database.getCollection("usuarios").updateOne(filtro, novoUsuario);
 
         if(resultado.getModifiedCount() > 0) {
-            System.out.println("Usuário alterado\n");
+            return true;
         } else {
-            System.out.println("Nenhum usuário encontrado\n");
+            return false;
         }
     }
 
     public void removerUsuario(Usuario usuario){
         Bson filtro = Filters.eq("cpf", usuario.getCpf());
-        DeleteResult resultado = colecaoUsuario.deleteOne(filtro);
+        DeleteResult resultado = database.getCollection("usuarios").deleteOne(filtro);
 
         if (resultado.getDeletedCount() > 0) {
             System.out.println("Usuário removido\n");
         } else {
             System.out.println("Nenhum usuário encontrado\n");
+        }
+    }
+
+    public Usuario buscarUsuarioPorEmailSenha(String email, String senha) {
+        Bson filtro = Filters.and(
+            Filters.eq("email", email),
+            Filters.eq("senha", senha)
+        );
+    
+        Document resultado = database.getCollection("usuarios").find(filtro).first();
+    
+        if (resultado != null) {
+            System.out.println("Usuário encontrado\n");
+            Usuario usuarioEncontrado = new Usuario();
+            usuarioEncontrado.setNome(resultado.getString("nome"));
+            usuarioEncontrado.setCpf(resultado.getString("cpf"));
+            usuarioEncontrado.setEmail(resultado.getString("email"));
+            usuarioEncontrado.setSenha(resultado.getString("senha"));
+    
+            return usuarioEncontrado;
+        } else {
+            System.out.println("Usuário não encontrado\n");
+            return null;
         }
     }
 }
